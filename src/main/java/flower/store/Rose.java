9085b78d448c014cb9fbfed19d349f3eb19074ff
YarynaFialko <92580927+YarynaFialko@users.noreply.github.com>
2022-10-22 23:22:59 +0300

    linter fixes

--- src/main/java/flower/store/Rose.java
+++ src/main/java/flower/store/Rose.java
@@ -2,8 +2,15 @@ package flower.store;
 
 
 public class Rose extends Flower {
+    /**
+     * FlowerType of Rose is ROSE.
+     */
     private final FlowerType flowerType = FlowerType.ROSE;
 
+    /**
+     * Returns a String value of the type.
+     * @return a String with the flower's type.
+     */
     public String getType() {
         return flowerType.toString();
     }
